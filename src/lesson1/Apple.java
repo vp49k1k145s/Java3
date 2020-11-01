02c7ebb9e1840220979d5bbbc3527dba1de77773
vp49k1k145s <vp49k1k145s@mail.ru>
2020-11-01 19:49:14 +0300

    ДЗ1

---
+++ src/lesson1/Apple.java
@@ -0,0 +1,7 @@
+package lesson1;
+
+public class Apple extends Fruit {
+    public Apple() {
+        super(1f);
+    }
+}
---
+++ src/lesson1/Fruit.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public abstract class Fruit {
+    protected float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
