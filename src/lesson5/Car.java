efd7d5eff9739c5125f24832d76f62cfc3ca5112
vp49k1k145s <vp49k1k145s@mail.ru>
2020-11-15 18:17:28 +0300

    ДЗ5

---
+++ src/lesson5/Car.java
@@ -0,0 +1,55 @@
+package lesson5;
+
+class Car implements Runnable {
+    private static int CARS_COUNT;
+
+    static {
+        CARS_COUNT = 0;
+    }
+
+    private Race race;
+    private int speed;
+    private String name;
+
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+
+            MainClass.START.countDown();
+            MainClass.START.await();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+
+        if (MainClass.FINISH.getCount() == MainClass.CARS_COUNT)
+            System.out.println(name + " - Выйграл");
+
+        try {
+            MainClass.FINISH.countDown();
+            MainClass.FINISH.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
